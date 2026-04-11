d1e314be52ac986f66c05cb561757b94043c270a
mrashed21 <rashedjaman768@gmail.com>
2026-04-11 17:31:16 +0600

    set-up server

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from "cors";
+import express, { Application } from "express";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+
+export default app;
